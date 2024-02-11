723f61fff6aaba07025266ae9c32c1b3c6fe2137
Dipongkar Roy <dipongkarroy233@gmail.com>
2024-02-11 20:06:01 +0600

    server project

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+//midleWire :
+app.use(cors())
+app.use(express.json())
+
+
+
+//mongoDb connected :
+
+const uri = "mongodb+srv://userDB:DGVgWYNegt1EDCLX@cluster0.bqchovi.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const dataBase = client.db("userDb");
+        const userConection = dataBase.collection("users");
+
+
+        app.get('/users', async (req, res) => {
+            const cursor = userConection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('new user add', user)
+            const result = await userConection.insertOne(user)
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id)
+            const query = { _id: new ObjectId(id) };
+            const result = await userConection.deleteOne(query);
+            res.send(result)
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('simple crud server is running')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`server runing: ${port}`)
+})
